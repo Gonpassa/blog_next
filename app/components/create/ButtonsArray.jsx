724c7d721e84e16985ea5104c5f629c0bcb8760f
Gonpassa <Gonzalopassa@hotmail.com>
2023-07-24 13:28:03 -0400

    styling for buttons

--- app/components/create/ButtonsArray.jsx
+++ app/components/create/ButtonsArray.jsx
@@ -1,14 +1,30 @@
 export default function AddButtons({ addInput }) {
     return (
-        <div>
-            <button onClick={(e) => addInput(e.target.innerText)}>
+        <div className="flex justify-between w-full mt-8">
+            <button
+                className="rounded-md bg-zinc-50 px-2.5 py-2 text-sm font-semibold text-zinc-150 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 dark:text-zinc-950"
+                onClick={(e) => addInput(e.target.innerText)}
+            >
                 Heading
             </button>
-            <button onClick={(e) => addInput(e.target.innerText)}>
+            <button
+                className="rounded-md bg-zinc-50 px-2.5 py-2 text-sm font-semibold text-zinc-150 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 dark:text-zinc-950"
+                onClick={(e) => addInput(e.target.innerText)}
+            >
                 Subheading
             </button>
-            <button onClick={(e) => addInput(e.target.innerText)}>Body</button>
-            <button onClick={(e) => addInput(e.target.innerText)}>Image</button>
+            <button
+                className="rounded-md bg-zinc-50 px-2.5 py-2 text-sm font-semibold text-zinc-150 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 dark:text-zinc-950"
+                onClick={(e) => addInput(e.target.innerText)}
+            >
+                Body
+            </button>
+            <button
+                className="rounded-md bg-zinc-50 px-2.5 py-2 text-sm font-semibold text-zinc-150 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 dark:text-zinc-950"
+                onClick={(e) => addInput(e.target.innerText)}
+            >
+                Image
+            </button>
         </div>
     );
 }
